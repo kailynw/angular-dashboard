d1a0777d9a6bfe8a16062eecec3d6e776cb59736
dnapoli97 <dnapoli1997@yahoo.com>
2020-03-15 16:22:47 -0400

    scrape2json.py path bug fix

--- server/data/scrape2json.py
+++ server/data/scrape2json.py
@@ -1,17 +1,18 @@
 import csv, json, pathlib
 
-csvFilePath = str(pathlib.Path('data.csv').parent.absolute()) + '/data.csv'
-jsonFilePath = str(pathlib.Path('data.json').parent.absolute()) + '/data.json'
+
+csvFilePath = pathlib.Path('data.csv').parent.absolute()
+jsonFilePath = pathlib.Path('data.json').parent.absolute()
 
 def main():
     data = {}
-    with open(csvFilePath) as csvFile:
+    with open(csvFilePath.as_posix() + '/server/data/data.csv') as csvFile:
         csvReader = csv.DictReader(csvFile, delimiter='|')
         for row in csvReader:
             id = row['Date']
             data[id]= row
 
-    with open(jsonFilePath, 'w') as jsonFile:
+    with open(jsonFilePath.as_posix() + '/server/data/data.json', 'w') as jsonFile:
         jsonFile.write(json.dumps(data, indent=4))
 
 main()
